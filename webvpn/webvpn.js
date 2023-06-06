177cd0880fc08e7821815747c3f667946b038ef3
wangyilin26 <wangyilin26@meituan.com>
2023-06-06 16:10:13 +0800

    Update webvpn.js

--- webvpn/webvpn.js
+++ webvpn/webvpn.js
@@ -1,8 +1,8 @@
 function convertOnClick()
 {
     const url = document.getElementById("url-input").value;
-    const key_iv = CryptoJS.enc.Utf8.parse("wrdvpnisthebest!");
-    const res = "https://webvpn.neu.edu.cn/http/77726476706e69737468656265737421"
+    const key_iv = CryptoJS.enc.Utf8.parse("b0A58a69394ce73@");
+    const res = "https://webvpn.neu.edu.cn/http/62304135386136393339346365373340"
     + CryptoJS.AES.encrypt(
         url, 
         key_iv, {
